14455f340c698849e72db2e956d54e0cf32fb9c0
David Lara <dlara7913@gmail.com>
2023-12-04 19:54:29 -0800

    -Added functions to encrypt and decrypt text files.

--- src/AES256Utility.java
+++ src/AES256Utility.java
@@ -1,11 +1,12 @@
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.interfaces.PBEKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -30,6 +31,7 @@ public class AES256Utility {
 
 
     /**
+     * Generate an IV that is going to be used for AES encryption
      * @return IvParameterSpec
      */
     public static IvParameterSpec generateIV(){
@@ -42,7 +44,89 @@ public class AES256Utility {
     }
 
 
-    public static void encryptFile(){
+    /**
+     * Takes in an inputFile and decrypts the file, the outputFile is the file that gets encrypted.
+     * @param algorithm The algorithm we want to use to encrypt.
+     * @param key The key we are using for encryption.
+     * @param IV The initialization vector.
+     * @param inputFile
+     * @param outputFile
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
+    public static void encryptFile(String algorithm, SecretKey key, IvParameterSpec IV, File inputFile, File outputFile) throws InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, IOException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher encryptionCipher = Cipher.getInstance(algorithm);
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key, IV);
+        FileInputStream fis = new FileInputStream(inputFile);
+        FileOutputStream fos = new FileOutputStream(outputFile);
+
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ( (bytesRead = fis.read(buffer)) != -1){
+            byte[] output = encryptionCipher.update(buffer, 0, bytesRead);
+            if (output != null){
+                fos.write(output);
+            }
+        }
+
+        byte[] outputBytes = encryptionCipher.doFinal();
+        if (outputBytes != null){
+            fos.write(outputBytes);
+        }
+
+        fis.close();
+        fos.close();
+
+
+
+
+    }
+
+    /**
+     * Takes in an inputFile and decrypts the file, the outputFile is the file that gets encrypted.
+     * @param algorithm The algorithm used for encryption.
+     * @param key The key used for encryption.
+     * @param IV The initialization vector used for encryption.
+     * @param inputStream The input file.
+     * @param outputStream The output file.
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws IOException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
+    public static void decryptFile(String algorithm, SecretKey key, IvParameterSpec IV, File inputStream, File outputStream) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException {
+        Cipher decryptionCipher = Cipher.getInstance(algorithm);
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, IV);
+        FileInputStream fis = new FileInputStream(inputStream);
+        FileOutputStream fos = new FileOutputStream(outputStream);
+
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ( (bytesRead = fis.read(buffer)) != -1){
+            byte[] output = decryptionCipher.update(buffer, 0, bytesRead);
+            if (output != null){
+                fos.write(output);
+            }
+        }
+
+        byte[] outputBytes = decryptionCipher.doFinal();
+        if (outputBytes != null){
+            fos.write(outputBytes);
+        }
+
+        fis.close();
+        fos.close();
+
+
 
     }
 
