440708e209ae262d8889b675bf64b6d025f17f0c
David Lara <dlara7913@gmail.com>
2023-12-04 16:34:35 -0800

    -Container for a Person that contains their RSA public and private key pairs.

--- Person.java
+++ Person.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    //RSA Public Key Variable
+
+    //RSA Private Key Variable
+
+    //Other user's public RSA Key
+
+    //Constructor to generate Public Key and Private Key Pair
+
+    //Encrypt Message using AES key
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Person {
+import java.security.*;
+import java.security.spec.RSAPrivateKeySpec;
+
+public class Person {
+    private KeyPairGenerator pairGenerator;
+    private KeyPair keyPair;
+
+
+    //RSA Public key pair
+    private PublicKey pubKey;
+
+
+    //RSA Private key pair
+    private PrivateKey privKey;
+
+
+    /** This Person constructor is used when there is no existing RSA private and public key pair for this person.
+     * In that event, new keys are generated, and then written to their own separate files.
+     * @throws NoSuchAlgorithmException
+     */
+    public Person() throws NoSuchAlgorithmException {
+
+        pairGenerator = KeyPairGenerator.getInstance("RSA");
+        //Size of key for RSA will be 2048 bits
+        pairGenerator.initialize(2048);
+        keyPair = pairGenerator.generateKeyPair();
+        this.pubKey = keyPair.getPublic();
+        this.privKey = keyPair.getPrivate();
+
+
+
+    }
+
+
+    /**This Person constructor is used when there is an existing RSA private and public key pair.
+     * In this case, we will read in the public key and store it into this person's private and public key pair.
+     * @param filePublicKey The read in public key from the file.
+     * @param filePrivateKey The read in private key from the file.Only
+     */
+    public Person(PublicKey filePublicKey, PrivateKey filePrivateKey){
+        this.pubKey = filePublicKey;
+        this.privKey = filePrivateKey;
+
+    }
+
+
+    public PrivateKey returnPrivateKey(){
+        return this.privKey;
+    }
+
+    public PublicKey returnPublicKey(){
+        return this.pubKey;
+    }
+
+
 }
