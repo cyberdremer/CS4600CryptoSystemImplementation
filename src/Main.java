b94ad73d43417c16ae66a726f38e0f540ec301e4
David Lara <dlara7913@gmail.com>
2023-12-05 09:48:36 -0800

    -Removed unassociated file writing and reading functions and moved them to their associated classes.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.*;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.security.*;
 import java.security.spec.EncodedKeySpec;
@@ -16,7 +15,7 @@ public class Main {
         Scanner keyboardInput = new Scanner(System.in);
         int userChoice;
         System.out.println("What operation would you like to do?" +
-                "\nLoad in a key-pair? (1)" +
+                "\nLoad in an RSA key-pair? (1)" +
                 "\nGenerate a new key-pair (2)" +
                 "\nExit the program (3)");
        boolean exit = false;
@@ -25,13 +24,13 @@ public class Main {
         userChoice = keyboardInput.nextInt();
         switch (userChoice){
             case 1:
-                PublicKey publicKey = loadInPublicKey(keyboardInput);
-                PrivateKey privateKey = loadInPrivateKey(keyboardInput);
+                PublicKey publicKey = RSAUtility.loadInPublicKey(keyboardInput);
+                PrivateKey privateKey = RSAUtility.loadInPrivateKey(keyboardInput);
                 currentPerson = new Person(publicKey,privateKey);
 
             case 2:
                 currentPerson = new Person();
-                writeOutKeys(currentPerson);
+                RSAUtility.writeOutKeys(currentPerson);
 
             default:
                 exit = true;
@@ -50,7 +49,9 @@ public class Main {
                 case 1:
                     //TODO function that uses AES 128 to encrypt a file
                 case 2:
-                    //TODO function that decrypts the AES key, and subsequently the FILE
+                    /*
+                    * TODO ask the user to import an RSA public key, then recreate the AES secret key key, then decrypt the file.
+                    * */
                 case 3:
                     //TODO Quit the program
             }
@@ -73,81 +74,8 @@ public class Main {
 
 
 
-    public static void writeOutKeys(Person person) throws FileNotFoundException {
-        try(FileOutputStream fos = new FileOutputStream("public.key")){
-            fos.write(person.returnPublicKey().getEncoded());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try(FileOutputStream fos = new FileOutputStream("private.key")){
-            fos.write(person.returnPrivateKey().getEncoded());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 
 
-    /**
-     * \Takes in user input to load in a file containing the public key for the RSA encryption scheme.
-     * @param keyboardInput
-     * @return PublicKey object
-     * @throws IOException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     */
-    public static PublicKey loadInPublicKey(Scanner keyboardInput) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        File keyFile;
-        byte[] publicKeyBytes;
-        String keyFileDirectory;
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        System.out.println("Enter the file directory of the key file");
-        keyFileDirectory = keyboardInput.nextLine();
-        keyFile = new File(keyFileDirectory);
-        if (keyFile.isFile()) {
-                publicKeyBytes = Files.readAllBytes(keyFile.toPath());
-                EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-                PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
-                return publicKey;
-
-
-
-        } else {
-            throw new FileNotFoundException();
-
-        }
-    }
-
-    /**
-     * Takes in user input to load in a file containing the private key for the RSA encryption scheme.
-     * @param keyboardInput Scanner object
-     * @return PrivateKey object
-     * @throws IOException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     */
-    public static PrivateKey loadInPrivateKey(Scanner keyboardInput) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        File keyFile;
-        byte[] publicKeyBytes;
-        String keyFileDirectory;
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        System.out.println("Enter the file directory of the key file");
-        keyFileDirectory = keyboardInput.nextLine();
-        keyFile = new File(keyFileDirectory);
-        if (keyFile.isFile()) {
-            publicKeyBytes = Files.readAllBytes(keyFile.toPath());
-            EncodedKeySpec privateKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-            PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
-
-            return privateKey;
-
-
-        } else {
-            throw new FileNotFoundException();
-
-        }
-    }
 
 
 
